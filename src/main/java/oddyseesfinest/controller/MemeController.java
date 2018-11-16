992eb638bd7c0277d93d34c0552ee6c0df73662f
Aapo Salo <aapojasalo@gmail.com>
2018-11-16 06:18:08 +0200

    removed postgres integration

--- src/main/java/oddyseesfinest/controller/MemeController.java
+++ src/main/java/oddyseesfinest/controller/MemeController.java
@@ -1,13 +1,20 @@
 package oddyseesfinest.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import oddyseesfinest.dao.MemeDao;
 import oddyseesfinest.model.Meme;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class MemeController {
@@ -29,6 +36,16 @@ public class MemeController {
 	@GetMapping("/test")
 	public String test() {
 		return memeDao.test();
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<Map<?, ?>> handleFileUpload(@RequestParam("file") MultipartFile file) throws Exception {
+
+		Map response;
+
+		response = memeDao.save(file);
+
+		return new ResponseEntity<Map<?, ?>>(response, null, HttpStatus.CREATED);
 	}
 
 }
--- src/main/java/oddyseesfinest/dao/MemeDao.java
+++ src/main/java/oddyseesfinest/dao/MemeDao.java
@@ -1,10 +1,14 @@
 package oddyseesfinest.dao;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.cloudinary.*;
+import com.cloudinary.utils.ObjectUtils;
 
 import oddyseesfinest.model.Meme;
 
@@ -17,6 +21,20 @@ public class MemeDao {
 
 		return new Meme();
 
+	}
+
+	public Map<?, ?> save(MultipartFile file) throws Exception {
+
+		Map<?, ?> uploadResult;
+
+		try {
+			uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+
+		} catch (Exception e) {
+			throw e;
+		}
+
+		return uploadResult;
 	}
 
 	public String test() {
